2aa0c29531dbf096ed3bdaa35ad28c19c0c47602
Roy Macdonald <macdonald.roy@gmail.com>
2017-04-14 20:49:42 -0300

    Updated apothecary script. - Stuff for linux taken from ofxDlib by @bakercp - osx version pulls from a modified fork that fixes "nil" issue on osx.

--- scripts/formulas/dlib.sh
+++ scripts/formulas/dlib.sh
@@ -12,16 +12,23 @@ GIT_URL="https://github.com/davisking/dlib"
 #GIT_TAG="v$VER"
 GIT_TAG=
 
-FORMULA_TYPES=( "osx" "android")
-
+FORMULA_TYPES=( "osx" "android" "linux64")
 
 # download the source code and unpack it into LIB_NAME
 function download() {
-	curl -Lk http://dlib.net/files/dlib-$VER.tar.bz2 -o dlib-$VER.tar.bz2
-	tar -xjvf dlib-$VER.tar.bz2
-	mv dlib-$VER dlib
-	rm dlib-$VER.tar.bz2
-
+	if [ "$TYPE" == "osx" ] ; then
+		#this is to fix an issue when compiling for apple machines. To be removed once the PR gets merged.
+		curl -Lk http://github.com/roymacdonald/dlib/archive/fix-nil-mac.zip -o dlib-fix-nil-mac.zip
+		unzip dlib-fix-nil-mac.zip
+		mv dlib-fix-nil-mac dlib
+		rm dlib-fix-nil-mac.zip
+	else
+		# curl -Lk http://dlib.net/files/dlib-$VER.tar.bz2 -o dlib-$VER.tar.bz2
+		# tar -xjvf dlib-$VER.tar.bz2
+		# mv dlib-$VER dlib
+		# rm dlib-$VER.tar.bz2
+		git clone $GIT_URL
+	fi
 }
 
 # prepare the build environment, executed inside the lib src dir
@@ -33,19 +40,17 @@ function prepare() {
 
 # executed inside the lib src dir
 function build() {
-  if [ "$TYPE" == "osx" ] ; then
+  if [ "$TYPE" == "osx" ] || [ "$TYPE" == "linux64" ] ; then
 		cd dlib
-		#if [ -d  builds ]; then
-		mkdir build
-		#fi
+		mkdir -p build
 		cd build
-
+		export MAKEFLAGS="-j$PARALLEL_MAKE -s"
+		cmake -D DLIB_NO_GUI_SUPPORT=yes -D CMAKE_INSTALL_PREFIX=$LIBS_DIR/dlib/install ..
 		cmake ..
 		cmake --build . --config Release
 		cd ../../
 	elif [ "$TYPE" == "android" ] ; then
 		${NDK_ROOT}/ndk-build -j4 NDK_DEBUG=0 NDK_PROJECT_PATH=.
-
 	fi
 }
 # executed inside the lib src dir, first arg $1 is the dest libs dir root
@@ -56,19 +61,18 @@ function copy() {
 	fi
 	mkdir -p $1/include
 
-	if [ "$TYPE" == "osx" ] ; then
-	    cp -vr dlib/ $1/include/dlib
-	    rm -rf $1/include/dlib/build
-	    rm -rf $1/include/dlib/test
-	    rm  $1/include/dlib/all_gui.cpp
-
-	    cp -v dlib/build/libdlib.a $1/lib/osx/libdlib.a
+if [ "$TYPE" == "osx" ] || [ "$TYPE" == "linux64" ] ; then
+		cd $BUILD_DIR/dlib/dlib/build
+		make install
+		cd -
+		mv $1/install/lib/libdlib.a $1/lib/$TYPE/
+		mv $1/install/include/dlib $1/include/
+		rm -r $1/install
 	elif [ "$TYPE" == "android" ] ; then
 	    cp -vr dlib/ $1/include/dlib
-			rm -rf $1/include/dlib/build
+		rm -rf $1/include/dlib/build
 	    rm -rf $1/include/dlib/test
 	    rm  $1/include/dlib/all_gui.cpp
-
 	    cp -vr obj/local/armeabi-v7a/libdlib.a $1/lib/android/armeabi-v7a/libdlib.a
 	    cp -vr obj/local/x86/libdlib.a $1/lib/android/x86/libdlib.a
 	fi
@@ -77,17 +81,13 @@ function copy() {
 # executed inside the lib src dir
 function clean() {
 	if [ "$TYPE" == "osx" ] ; then
-
    		cd dlib/build
    		cmake clean .
    		cd ..
    		rm -rf build
 		cd ..
-
 	elif [ "$TYPE" == "android" ] ; then
 		rm -rf obj
 	fi
-
-
 
 }
